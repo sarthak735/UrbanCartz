212c6e8d0fbc3c82e53ef9e7f81e866d4199e571
Kunwar Sarthak Singh <kuwarsarthaksingh735@gmail.com>
2024-05-09 18:35:01 +0530

    Added same render endpoints

--- admin/src/Components/ListProduct/ListProduct.jsx
+++ admin/src/Components/ListProduct/ListProduct.jsx
@@ -5,7 +5,7 @@ const ListProduct = () => {
   const [allProducts, setAllProducts] = useState([]);
 
   const fetchInfo = async () => {
-    await fetch("http://localhost:4000/allproducts")
+    await fetch("https://urbancartz-backend.onrender.com/allproducts")
       .then((res) => res.json())
       .then((data) => {
         setAllProducts(data);
@@ -17,7 +17,7 @@ const ListProduct = () => {
   }, []);
 
   const remove_product = async (id) => {
-    await fetch("http://localhost:4000/removeproduct", {
+    await fetch("https://urbancartz-backend.onrender.com/removeproduct", {
       method: "POST",
       headers: {
         Accept: "application/json",
